5277c5a37868168e3f32fbc7157546694e5abcce
Guillaume377 <guillaumestephan@live.fr>
2023-06-20 16:30:46 +0200

    migrations, seeders et modèles en place

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,36 +10,66 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    // use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    // /**
+    //  * The attributes that are mass assignable.
+    //  *
+    //  * @var array<int, string>
+    //  */
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    // ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    // /**
+    //  * The attributes that should be hidden for serialization.
+    //  *
+    //  * @var array<int, string>
+    //  */
+    // protected $hidden = [
+    //     'password',
+    //     'remember_token',
+    // ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    // /**
+    //  * The attributes that should be cast.
+    //  *
+    //  * @var array<string, string>
+    //  */
+    // protected $casts = [
+    //     'email_verified_at' => 'datetime',
+    //     'password' => 'hashed',
+    // ];
+
+
+
+
+    // nom au pluriel car un user peut poster plusieurs quacks
+    //cardinalité 0,n
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    // nom au pluriel car un user peut poster plusieurs commentaires
+    // cardinalité 0,n
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    // nom de la fonction au singulier car 1 seul rôle en relation
+    // cardinalité 1,1
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function isAdmin()
+    {
+        //return $this->role == "admin";
+        return $this->role_id == 2;
+    }
 }
